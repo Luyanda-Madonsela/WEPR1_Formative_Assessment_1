d173b178d42273b1ea13599bde14e725e575decd
Luyanda Madonsela <luyandasmadonsela@gmail.com>
2023-08-20 10:06:41 +0200

    Dockerfile

--- my-app/src/tests/Navigation.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Navigation from '../components/Navigation';
-
-it('should render the game header', () => {
-  render(<Navigation />);
-  const header = screen.getByText('TIC-TAC-TOE');
-  expect(header).toBeInTheDocument();
-});
